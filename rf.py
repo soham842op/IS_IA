c7cb05f604f609db23317cedf64b432d49728bd7
omkark4004 <84176722+omkark4004@users.noreply.github.com>
2023-02-18 20:59:59 +0530

    Update rf.py

--- rf.py
+++ rf.py
@@ -262,6 +262,83 @@ def extract_feature_train(url,output):
     #output
     yn = output
 
-   
-        
     return yn,length_of_url,http_has,suspicious_char,prefix_suffix,dots,slash,phis_term,sub_domain,ip_contain
+
+#import train data
+def importdata_train(): 
+    balance_data = pd.read_csv('feature_train.csv',sep= ',', header = 1,usecols=range(1,11),encoding='utf-8') 
+      
+      
+    # Printing the dataswet shape 
+    print ("Dataset Lenght: ", len(balance_data)) 
+    print ("Dataset Shape: ", balance_data.shape) 
+      
+    # Printing the dataset obseravtions 
+    print ("Dataset: ",balance_data.head()) 
+    return balance_data 
+#import test data
+def importdata_test(): 
+    balance_data = pd.read_csv('feature_test.csv',sep= ',', header = 1,usecols=range(1,11),encoding='utf-8') 
+      
+      
+    # Printing the dataswet shape 
+    print ("Dataset Lenght: ", len(balance_data)) 
+    print ("Dataset Shape: ", balance_data.shape) 
+      
+    # Printing the dataset obseravtions 
+    print ("Dataset: ",balance_data.head()) 
+    return balance_data 
+#split data into train and test
+def splitdataset(balance_data): 
+  
+    # Seperating the target variable 
+    X = balance_data.values[:, 1:10]
+    Y = balance_data.values[:, 0] 
+  
+    # Spliting the dataset into train and test 
+    #X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3, random_state = 100) 
+      
+    return X, Y
+# Function to perform training with entropy. 
+def tarin_using_entropy(X_train, y_train): 
+  
+    # Decision tree with entropy 
+    clf_entropy = DecisionTreeClassifier( 
+            criterion = "entropy", random_state = 100, 
+            max_depth = 2, min_samples_leaf = 10) 
+  
+    # Performing training 
+    clf_entropy.fit(X_train, y_train) 
+    return clf_entropy 
+# Function to make predictions 
+def prediction(X_test, clf_object): 
+  
+    # Predicton on test with giniIndex 
+    y_pred = clf_object.predict(X_test) 
+    #print("Predicted values:") 
+    #print(y_pred) 
+    return y_pred 
+# Function to calculate accuracy 
+def cal_accuracy(y_test, y_pred): 
+      
+    print("Confusion Matrix: ", 
+        confusion_matrix(y_test, y_pred)) 
+      
+    print ("Accuracy : ", 
+    accuracy_score(y_test,y_pred)*100) 
+      
+    print("Report : ", 
+    classification_report(y_test, y_pred))
+
+    return accuracy_score(y_test,y_pred)*100
+
+#roc
+def plot_roc_curve(fpr, tpr ):  
+    plt.plot(fpr, tpr, color='orange', label='ROC')
+    plt.plot([0, 1], [0, 1], color='darkblue', linestyle='--')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic (ROC) Curve')
+    plt.legend()
+    plt.show()
+
